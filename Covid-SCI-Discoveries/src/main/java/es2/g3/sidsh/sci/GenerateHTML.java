e7157b7be6f89657bbc08dc15c0779f7c41f9257
dspaa1-iscteiul <dspaa1@iscte-iul.pt>
2020-06-15 00:34:38 +0100

    Re-upload projeto

---
+++ Covid-SCI-Discoveries/src/main/java/es2/g3/sidsh/sci/GenerateHTML.java
@@ -0,0 +1,49 @@
+package es2.g3.sidsh.sci;
+
+import java.util.ArrayList;
+
+import cgi.cgi_lib;
+
+public class GenerateHTML {
+
+	public static void main(String[] args) {
+
+		Covid_SCI_Discoveries cermine;
+		if (args.length != 0) {
+			cermine = new Covid_SCI_Discoveries(args[0]);
+		} else {
+			cermine = new Covid_SCI_Discoveries("./");
+		}
+
+		ArrayList<Article> articles = cermine.generateArticles();
+
+		System.out.println(cgi_lib.Header());
+		System.out.println(cgi_lib.HtmlTop("Covid Scientific Discoveries Repository"));
+		System.out.println("<h1>Covid Scientific Discoveries Repository<h1>");
+		System.out.println("<table style=\"width:85%\" cellspacing=\"1\" border=\"1\">");
+		System.out.println("<tr>");
+		System.out.println("<th>Article title</th>");
+		System.out.println("<th>Journal name</th>");
+		System.out.println("<th>Publication year</th>");
+		System.out.println("<th>Authors</th>");
+		System.out.println("</tr>");
+
+		if (args.length == 0) {
+			System.out.println("<h2>Não foi especificado o caminho para os ficheiros</h2>");
+		} else {
+			for (Article article : articles) {
+				System.out.println("<tr>");
+				System.out.println("<td><a href=\"" + cermine.getArticles_folder() + article.getFileName() + "\">"
+						+ article.getTitle() + "</a></td>");
+				System.out.println("<td>" + article.getJournal_name() + "</td>");
+				System.out.println("<td>" + article.getPub_year() + "</td>");
+				System.out.println("<td>" + article.getAuthorsString() + "</td>");
+				System.out.println("</tr>");
+			}
+		}
+		System.out.println("</table>\n");
+		System.out.println(cgi_lib.HtmlBot());
+
+	}
+
+}
---
+++ Covid-SCI-Discoveries/src/test/java/es2/g3/sidsh/sci/GenerateHTMLTest.java
@@ -0,0 +1,56 @@
+/**
+ * 
+ */
+package es2.g3.sidsh.sci;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+/**
+ * @author dariop
+ *
+ */
+class GenerateHTMLTest {
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@BeforeAll
+	static void setUpBeforeClass() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@AfterAll
+	static void tearDownAfterClass() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@AfterEach
+	void tearDown() throws Exception {
+	}
+
+	/**
+	 * Test method for {@link es2.g3.sidsh.sci.GenerateHTML#main(java.lang.String[])}.
+	 */
+	@Test
+	void testMain() {
+		fail("Not yet implemented");
+	}
+
+}
