98b1e13e9bb6b35ca8b105e87814c8e5f4b0974f
rmdca-iscteiul <rmdca@iscte-iul.pt>
2020-06-15 03:30:06 +0100

    teste 123

---
+++ covid-evolution-diff/src/test/java/ESII_G3_SIDSH/covid_evolution_diff/Covid_evolution_diffTest.java
@@ -0,0 +1,35 @@
+package ESII_G3_SIDSH.covid_evolution_diff;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class Covid_evolution_diffTest {
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		Covid_evolution_diff c = new Covid_evolution_diff();
+		assertNotNull(c);
+	}
+
+}
